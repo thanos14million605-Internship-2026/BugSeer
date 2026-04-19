fb05c9c6a5cdcbae0d9a8d9181f4272761010977
thanos14million605 <gajaga.dev@example.com>
2026-04-19 12:13:52 +0530

    Updated database.js

--- backend/node/config/database.js
+++ backend/node/config/database.js
@@ -1,7 +1,10 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  connectionString:
+    process.env.NODE_ENV === "production"
+      ? process.env.DATABASE_URL_PROD
+      : process.env.DATABASE_URL,
   ssl:
     process.env.NODE_ENV === "production"
       ? { rejectUnauthorized: false }
